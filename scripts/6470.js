c75e767d36961953dcb48b36209f55fabe417b03
Ying Yin <clevershadow@gmail.com>
2010-10-20 23:32:16 -0400

    Added Cai's work in the repository.

---
+++ scripts/6470.js
@@ -0,0 +1,69 @@
+
+
+var currentSlide = 1;
+var numslides;
+
+var slideChanger = function(slideNumber) {
+	return function() {
+		changeSlide(slideNumber);
+		pauseCarousel();
+	}
+}
+
+var changeSlide = function(slideNumber) {
+	// get the outer width without margin
+	var slideWidth = $("div#carousel").outerWidth(); 
+	numslides = $("div.slide").length;
+	// where to start the slide
+	var startx = slideWidth;
+	// where to send the other slide
+	var endx = -slideWidth;
+	if(currentSlide > slideNumber) {
+		startx = -slideWidth; // start it on the left
+		endx = slideWidth;
+	}
+	$("div.slide_"+slideNumber).animate({
+		left:startx
+	}, 0);
+	$("div.slide_"+slideNumber).animate({
+		left:0
+	}, 500);
+	$("div.slide_"+currentSlide).animate({
+		left:endx
+	}, 500);
+	
+	currentSlide = slideNumber;
+};
+
+// pauses the carousel's auto sliding function
+// for a while.
+var pauseCarousel = function() {
+	clearTimeout(autoplaytimer);
+	autoplaytimer = setTimeout(autoPlay, 20000);
+}
+
+var autoplaytimer;
+var autoPlay = function() {
+	next = currentSlide + 1;
+	if(next > numslides)
+		next = 1;
+	changeSlide(next);
+	autoplaytimer = setTimeout(autoPlay, 5000);
+}
+
+
+
+$(document).ready(function () {
+	var slideWidth = $("div#carousel").outerWidth(); 
+	// change all but the first slide
+	console.log($("div.slide+div.slide"));
+	$("div.slide+div.slide").css({"left":slideWidth+"px"});
+	$("div.slide_1").css({"left":"0px"});
+	currentSlide = 1;
+	//changeSlide(2);
+	
+	for(var i = 1; i <= 4; i++)
+		$("a.slide_"+i).click(slideChanger(i));
+	
+	autoplaytimer = setTimeout(autoPlay, 5000);
+});
