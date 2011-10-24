7478f9b9fe8e3c3241afa2251bfda16f66632eb8
Joseph Laurendi <jlaurendi@gmail.com>
2011-10-23 23:31:32 -0400

    Fixed homepage

--- 2012/application/classes/controller/home.php
+++ 2012/application/classes/controller/home.php
@@ -1,9 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Home extends Controller_MIT6470Template {
+class Controller_Home extends Controller_IndexTemplate {
 
 	public function action_index()
 	{
+		$this->template->footer = '';
+		
 		$this->template->content = View::factory('home/index');
 	}
 
