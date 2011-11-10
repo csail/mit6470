efdb3d7962d68da7ee7fdbb37414e3272748ace8
Joseph Laurendi <jlaurendi@gmail.com>
2011-11-10 10:58:42 -0500

    Added hackathons file

---
+++ 2012/application/views/competition/hackathons.php
@@ -0,0 +1,10 @@
+<!-- Title / Page Headline -->
+<h1 class="headline center"><strong>Hackathons</strong></h1>
+
+<div class="hr"></div>
+
+<p>
+	This year we're introducing hackathons to 6.470.  Our current plan is to have two of them over IAP.
+	For each, we'll present you with a challenge that you'll be able to tackle in teams of up to 3. The 
+	overall winners will earn prizes. We'll update this site with more information later on.
+</p>
