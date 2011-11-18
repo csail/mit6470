405c4edc4adc7f065d1cbbc501ab6685bfdd0940
Joseph Laurendi <jlaurendi@gmail.com>
2011-11-18 09:15:50 -0500

    Fixed schedule; added google analytics

--- 2012/application/views/course/schedule.php
+++ 2012/application/views/course/schedule.php
@@ -45,14 +45,14 @@
 			<td class="matrixItem">Wednesday 1/11/12</td>
 			<td class="matrixOdd">11:00am - 12:00pm</td>
 			<td class="matrixEven">TBD</td>
-			<td class="matrixOdd">PHP</td>
+			<td class="matrixOdd">MySQL</td>
 			<td class="matrixEven"></td>			
 		</tr>
 		<tr>
 			<td class="matrixItem"><br /></td>
 			<td class="matrixOdd">12:00pm - 1:00pm</td>
 			<td class="matrixEven">TBD</td>
-			<td class="matrixOdd">MySQL</td>
+			<td class="matrixOdd">PHP</td>
 			<td class="matrixEven"></td>
 		</tr>
 		<tr>
@@ -70,13 +70,6 @@
 			<td class="matrixEven"></td>
 		</tr>
 		<tr>
-			<td class="matrixItem">1/17/12 - 1/20/12</td>
-			<td class="matrixOdd">11:00am - 3:00pm</td>
-			<td class="matrixEven">TBD</td>
-			<td class="matrixOdd">Guest Lectures TBD</td>
-			<td class="matrixEven">check back for exact schedule</td>
-		</tr>
-		<tr>
 			<td class="matrixItem"><br /></td>
 			<td class="matrixOdd">11:30am - 12:00pm</td>
 			<td class="matrixEven">TBD</td>
@@ -91,11 +84,11 @@
 			<td class="matrixEven"></td>
 		</tr>
 		<tr>
-			<td class="matrixItem">Friday 1/13/12</td>
-			<td class="matrixOdd">11:00am - 11:30am</td>
+			<td class="matrixItem">1/17/12 - 1/20/12</td>
+			<td class="matrixOdd">11:00am - 3:00pm</td>
 			<td class="matrixEven">TBD</td>
-			<td class="matrixOdd">JavaScript</td>
-			<td class="matrixEven"></td>
+			<td class="matrixOdd">Guest Lectures TBD</td>
+			<td class="matrixEven">check back for exact schedule</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">Sunday 1/15/12</td>
@@ -148,7 +141,7 @@
 		</tr>
 		<tr>
 			<td class="matrixItem">Sunday 1/29/12</td>
-			<td class="matrixOdd">12:00pm - 8:00pm</td>
+			<td class="matrixOdd">12:00pm - 4:00pm</td>
 			<td class="matrixEven">TBD</td>
 			<td class="matrixOdd">Milestone 3 + Office Hours</td>
 			<td class="matrixEven">none</td>
@@ -161,18 +154,18 @@
 			<td class="matrixEven">none</td>
 		</tr>
 		<tr>
-			<td class="matrixItem last">Tuesday 1/31/12</td>
-			<td class="matrixOdd last">6:00pm</td>
-			<td class="matrixEven last">N/A</td>
-			<td class="matrixOdd last">Final Submission Deadline</td>
-			<td class="matrixEven last">none</td>
+			<td class="matrixItem">Tuesday 1/31/12</td>
+			<td class="matrixOdd">6:00pm</td>
+			<td class="matrixEven">N/A</td>
+			<td class="matrixOdd">Final Submission Deadline</td>
+			<td class="matrixEven">none</td>
 		</tr>
 		<tr>
-			<td class="matrixItem last">Wednesday 2/1/12</td>
-			<td class="matrixOdd last">11:00am - 5:00pm</td>
-			<td class="matrixEven last">TBD</td>
-			<td class="matrixOdd last">Semifinalist Presentations</td>
-			<td class="matrixEven last">none</td>
+			<td class="matrixItem">Wednesday 2/1/12</td>
+			<td class="matrixOdd">11:00am - 5:00pm</td>
+			<td class="matrixEven">TBD</td>
+			<td class="matrixOdd">Semifinalist Presentations</td>
+			<td class="matrixEven">none</td>
 		</tr>
 		<tr>
 			<td class="matrixItem last">Thursday 2/2/12</td>
