36ca2ea00d40b9693f9fda041e8e24848a61df42
Joseph Laurendi <jlaurendi@gmail.com>
2011-12-17 17:37:56 -0500

    Added video js library

--- 2012/application/classes/controller/course.php
+++ 2012/application/classes/controller/course.php
@@ -11,6 +11,8 @@ class Controller_Course extends Controller_MIT6470Template {
 	}
 
 	public function action_video() {
+		$this->template->scripts[] = 'assets/js/video.js';
+		$this->template->styles['assets/css/video-js.css'] = 'screen';
 		$this->template->content = View::factory('course/video');		
 	}
 
