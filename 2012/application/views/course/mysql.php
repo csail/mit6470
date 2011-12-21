f7914c803382008a59e8224e2947ae65a43b4732
unknown <Mark Zhang@MarkZhang-HP.(none)>
2011-12-21 04:24:00 -0500

    fixed problem with video poster images

--- 2012/application/views/course/mysql.php
+++ 2012/application/views/course/mysql.php
@@ -32,7 +32,7 @@ if(isset($_GET["video"]))
 <div class="hr"></div>
 <div class="video-js-box">
   <!-- Using the Video for Everybody Embed Code http://camendesign.com/code/video_for_everybody -->
-  <video id="video" class="video-js" width="640" height="360" controls="controls" preload="auto" poster="/6470_web/2012/assets/images/6470_video_logo.png">
+  <video id="video" class="video-js" width="640" height="360" controls="controls" preload="auto" poster="/2012/assets/images/6470_video_logo.png">
 	
     <source src="<?php echo URL::site('assets/videos/web_overview/'.$VIDEO_FILE_NAME.'.mp4'); ?>" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
     <!-- <source src="http://video-js.zencoder.com/oceans-clip.webm" type='video/webm; codecs="vp8, vorbis"' />
@@ -42,9 +42,9 @@ if(isset($_GET["video"]))
       data="http://releases.flowplayer.org/swf/flowplayer-3.2.1.swf">
       <param name="movie" value="http://releases.flowplayer.org/swf/flowplayer-3.2.1.swf" />
       <param name="allowfullscreen" value="true" />
-      <param name="flashvars" value='config={"playlist":["/6470_web/2012/assets/images/6470_video_logo.png", {"url": "http://video-js.zencoder.com/oceans-clip.mp4","autoPlay":false,"autoBuffering":true}]}' />
+      <param name="flashvars" value='config={"playlist":["/2012/assets/images/6470_video_logo.png", {"url": "http://video-js.zencoder.com/oceans-clip.mp4","autoPlay":false,"autoBuffering":true}]}' />
       <!-- Image Fallback. Typically the same as the poster image. -->
-      <img src="/6470_web/2012/assets/images/6470_video_logo.png" width="640" height="264" alt="Poster Image"
+      <img src="/2012/assets/images/6470_video_logo.png" width="640" height="264" alt="Poster Image"
         title="No video playback capabilities." />
     </object>
   </video>
