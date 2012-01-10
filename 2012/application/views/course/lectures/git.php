99415ac1dc96b92a597ec208957efec1b606eff1
Joseph Laurendi <jlaurendi@gmail.com>
2012-01-10 12:54:46 -0500

    Added git tutorial

---
+++ 2012/application/views/course/lectures/git.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><META http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body><h1><strong>Revision Control System</strong></h1>
+<p>We want to encourage all teams to use Git for their revision control system needs or at least use <i>a</i> revision control system for their code. There are an number of revision control systems out there, Git, CVS, SVN, Mercurial... You are welcome to use any of these by the staff will be able to provide the most support for Git.</p>
+
+<h2><strong>Github</strong></h2>
+<p>
+If you do not want the overhead of setting up your own git repository, you can go to <a href="http://www.github.com" target="_blank">github.com </a> and it will set up a repository for you on their servers. Public repositories are free and if you would like a private one, please fill out this <a href="https://docs.google.com/spreadsheet/embeddedform?formkey=dEFyN0o0a0otM0FLWjFLQlJMTmVhQnc6MQ" target="_blank">form </a> and we will set you up with a private repository. </p>
+
+<h2><strong>Resources</strong></h2>
+<p>Git is very well documented. Here are some resources for getting started with git.
+<ul>
+    <li><a href="http://schacon.github.com/git/user-manual.html" target="_blank">Git user manual. </a></li>
+    <li><a href="http://help.github.com/win-set-up-git/" target="_blank">Setting up Github on windows. </a></li>
+    <li><a href="http://help.github.com/mac-set-up-git/" target="_blank">Setting up Github on mac. </a> </li>
+    <li><a href="http://help.github.com/linux-set-up-git/" target="_blank">Setting up Github on linux. </a> </li>
+    <li><a href="http://help.github.com/git-cheat-sheets/" target="_blank">Git cheat sheet. </a> </li>
+</ul>
+</p></body></html>
