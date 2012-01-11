1b2585a6c01d0843292a511bd88c3660651412b9
Joseph Laurendi <jlaurendi@gmail.com>
2012-01-11 10:43:48 -0500

    Modified milestones

--- 2012/application/views/competition/milestones.php
+++ 2012/application/views/competition/milestones.php
@@ -19,12 +19,12 @@
 		<tr>
 			<td class="matrixItem">Milestone 2</td>
 			<td class="matrixOdd">1/22/12</td>
-			<td class="matrixEven">TBA</td>
+			<td class="matrixEven">Dynamic Database-backed Functionality</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">Milestone 3</td>
 			<td class="matrixOdd">1/29/12</td>
-			<td class="matrixEven">TBA</td>
+			<td class="matrixEven">Non-trivial Frontend Feature</td>
 		</tr>
 		<tr>
 			<td class="matrixItem last">Final Submission</td>
@@ -36,14 +36,55 @@
 <br />
 
 <h3>Milestone 1 - UI/UX Design Mockup</h3>
-<p>Info coming soon... Apologies for the inconvenience.</p>
+<p>Think about your users. Who is likely to use your site? Come up with at least two different personas and one use case for your site.</p>
+
+<ul>
+	<li>Identify at least one user variable (technical expertise, familiarity with topic, frequency of visits).</li>
+	<li>Sit down with three people, and conduct user research interviews on each of them. Briefly summarize these interviews.</li>
+	<li>Use these interviews to develop your personas.</li>
+	<li>Identify one use case for your site.</li>
+</ul>
+
+<p>Think hard about your most complicated page. What are the important features of this page? What usability problems may come up? For your most import page:</p>
+
+<ul>
+	<li>Draw out, by hand, three designs for this page. Scan these for your submission.</li>
+	<li>Make a list of pros and cons for each design (3 pros and 3 cons for each).</li>
+	<li>Pick the best one and mock it up using a program such as Photoshop, Inkscape, Gimp...</li>
+</ul>
+
+<p>Additionally, please answer the following questions. Please be succinct as we</p>
+
+<ol>
+	<li>What problem does your application address? Aim for 1-2 sentences.</li>
+	<li>How does your application address the problem above? Aim for one sentence. The next question is a better place for elaborate explanations.</li>
+	<li>What is the killer feature of your application?</li>
+	<li>Which of the themes does your application match? Your answer should be one sentence if the match is reasonably obvious. Be as brief as you can. If your application matches both themes, list the one where the matching is more obvious.</li>
+	<li>What technology do you plan to use for your server-side programming (e.g. PHP, Ruby on Rails, etc)?</li>
+	<li>Who is on your team? You may list at most 3 people. For each member list the full legal name, .edu e-mail, school, major(s), year, and graduate/undergraduate status.</li>
+	<li>How did the team members meet? This will help us know you better.</li>
+	<li>What risks do you envision preventing you from successfully implementing your idea? Consider this an exercise of imagination, not a test of confidence.</li>
+	<li>Are you planning to participate in the competition? Your answer will solely be used for planning purposes.</li>
+</ol>
+
+<p>Submit a PDF (or a ZIP if you have multiple files) to <a href="http://stellar.mit.edu/S/course/6/ia12/6.s188/">Stellar</a> that addresses each of the bullet points and numbered questions above. Only one person from your group needs to submit this but make sure your teammates are listed.</p>
 
 <h3>Milestone 2 - Dynamic Database-backed Functionality</h3>
 <p>
-Coming soon...
+	For your website, we would like you to utilize a server-side programming language (e.g. PHP, Ruby, Python) along with a database system (e.g. MySQL) to be able to do two things, at a minimum:
+	<ul>
+		<li>User login - we would like you to implement the ability for users to signup and login to your website. Third party API's such as Facebook Connect or OpenID are allowed.</li>
+		<li>Dynamic Content - You must have at least one page whose content will change dynamically based on a button click, form selection, etc. The dynamic change must be due to server-side scripts and must utilize a database.</li>
+	</ul>
 </p>
 
-<h3>Milestone 3 - Non-trivial Frontend Feature</h3>
+<h3>Milestone 3 - Dynamic Frontend Feature</h3>
 <p>
-Coming soon...
+	In addition to the dynamic backend capabilities we required in milestone 2, we would also like for you to create a dynamic frontend feature. The options for this are numerous. Here are some examples:
 </p>
+
+<ul>
+	<li>Filtering/sorting done entirely in JavaScript.</li>
+	<li>Use of drag and drop to improve the UI.</li>
+	<li>Usage of either CSS3 or JavaScript transitions, animations, or transformations.</li>
+</ul>
