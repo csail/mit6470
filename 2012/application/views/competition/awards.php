0a1a071e347cdfc0d4a80714a33d560411dd2e3e
Joseph Laurendi <jlaurendi@gmail.com>
2012-02-12 10:32:12 -0500

    Updated awards list pt 2

--- 2012/application/views/competition/awards.php
+++ 2012/application/views/competition/awards.php
@@ -11,7 +11,7 @@ div.lecture_details_div h2 { text-align: center;}
 h2.award_header { text-align: center; font-weight: bold; margin-top: 20px;}
 </style>
 <script>
-$('a').attr('target', '_blank');
+$('a.team_site').attr('target', '_blank');
 </script>
 
 <h2 class="award_header">Top Awards</h2>
@@ -29,25 +29,25 @@ $('a').attr('target', '_blank');
 			<td class="matrixOdd">1st</td>
 			<td class="matrixEven">Asler</td>
 			<td class="matrixOdd">Stephan Boyer<br /> Rachel Fong <br/> Mark Wittels</td>
-			<td class="matrixEven"><a href="http://asler.org/">www.asler.org</a></td>
+			<td class="matrixEven"><a href="http://asler.org/" class="team_site">www.asler.org</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd">2nd</td>
 			<td class="matrixEven">In The Loop</td>
 			<td class="matrixOdd">Zack Drach<br /> Pasha Muravyev</td>
-			<td class="matrixEven"><a href="http://li327-143.members.linode.com/">li327-143.members.linode.com</a></td>
+			<td class="matrixEven"><a href="http://li327-143.members.linode.com/" class="team_site">li327-143.members.linode.com</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd">3rd</td>
 			<td class="matrixEven">CollegeChef</td>
 			<td class="matrixOdd">Stephanie Chang<br /> Stephanie Yu<br /> Victor Hung</td>
-			<td class="matrixEven"><a href="http://niwen.scripts.mit.edu/collegechef/tutorials">niwen.scripts.mit.edu/collegechef/tutorials</a></td>
+			<td class="matrixEven"><a href="http://niwen.scripts.mit.edu/collegechef/tutorials" class="team_site">niwen.scripts.mit.edu/collegechef/tutorials</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd last">4th</td>
 			<td class="matrixEven last">LearnTo</td>
 			<td class="matrixOdd last">Brodrick Dunlap Childs<br /> Oleksandr Stubailo<br /> Jeffrey Tyler Warren</td>
-			<td class="matrixEven last"><a href="http://learnto.herokuapp.com/">learnto.herokuapp.com</a></td>
+			<td class="matrixEven last"><a href="http://learnto.herokuapp.com/" class="team_site">learnto.herokuapp.com</a></td>
 		</tr>
 </table>
 <br />
@@ -65,26 +65,26 @@ $('a').attr('target', '_blank');
 		<tr>
 			<td class="matrixOdd">ProtoM</td>
 			<td class="matrixEven">Andrew Correa<br /> Jeremy Scott<br /> Ying Yin</td>
-			<td class="matrixOdd"><a href="http://miscoe.csail.mit.edu/">miscoe.csail.mit.edu</a></td>
+			<td class="matrixOdd"><a href="http://miscoe.csail.mit.edu/" class="team_site">miscoe.csail.mit.edu</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd">re:search</td>
 			<td class="matrixEven">Leah Alpert<br />
 			Russell Cohen<br />
 			Ram Bhaskaramurthi</td>
-			<td class="matrixOdd"><a href="http://rcoh.scripts.mit.edu/gradsearch/website/">rcoh.scripts.mit.edu/gradsearch/website</a></td>
+			<td class="matrixOdd"><a href="http://rcoh.scripts.mit.edu/gradsearch/website/" class="team_site">rcoh.scripts.mit.edu/gradsearch/website</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd">ViewerVerse</td>
 			<td class="matrixEven">Elisabeth Morant<br />
 			Ruby Tamberino<br />
 			Bridger Maxwell</td>
-			<td class="matrixOdd"><a href="http://www.viewerverse.com">www.viewerverse.com</a></td>
+			<td class="matrixOdd"><a href="http://www.viewerverse.com" class="team_site">www.viewerverse.com</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd last">VoteNotes</td>
 			<td class="matrixEven last">Andres Romero<br /> Vo Thanh Minh Tue<br /> Paul Hemberger</td>
-			<td class="matrixOdd last"><a href="http://minhtuev.scripts.mit.edu/">minhtuev.scripts.mit.edu</a></td>
+			<td class="matrixOdd last"><a href="http://minhtuev.scripts.mit.edu/" class="team_site" >minhtuev.scripts.mit.edu</a></td>
 		</tr>		
 </table>
 <br />
@@ -102,46 +102,46 @@ $('a').attr('target', '_blank');
 		<tr>
 			<td class="matrixOdd">Art Zone</td>
 			<td class="matrixEven">Cristina Lozano<br /> Sara Brockmueller</td>
-			<td class="matrixOdd"><a href="http://sarab609.scripts.mit.edu/">sarab609.scripts.mit.edu</a></td>
+			<td class="matrixOdd"><a href="http://sarab609.scripts.mit.edu/" class="team_site">sarab609.scripts.mit.edu</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd">Choose Your Chief</td>
 			<td class="matrixEven">Sharon Hao<br />
 			Britt Cyr<br />
 			Tim Donegan</td>
-			<td class="matrixOdd"><a href="http://chooseyourchief.com/home.php">www.chooseyourchief.com/home.php</a></td>
+			<td class="matrixOdd"><a href="http://chooseyourchief.com/home.php" class="team_site">www.chooseyourchief.com/home.php</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd">Hippol</td>
 			<td class="matrixEven">Andi Wang<br /> Xunjie Li<br /> Ruwen Liu</td>
-			<td class="matrixOdd"><a href="http://ruwenliu.scripts.mit.edu/">ruwenliu.scripts.mit.edu</a></td>
+			<td class="matrixOdd"><a href="http://ruwenliu.scripts.mit.edu/" class="team_site">ruwenliu.scripts.mit.edu</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd last">LessonDrop</td>
 			<td class="matrixEven last">Shri Ganeshram<br />
 			Mark Fayngersh</td>
-			<td class="matrixOdd last"><a href="http://www.lessondrop.com">www.lessondrop.com</a></td>
+			<td class="matrixOdd last"><a href="http://www.lessondrop.com" class="team_site">www.lessondrop.com</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd">LucCo</td>
 			<td class="matrixEven">Michelle Fung<br /> Quanquan Liu</td>
-			<td class="matrixOdd"><a href="http://mfung.scripts.mit.edu/LucCo/">mfung.scripts.mit.edu/LucCo</a></td>
+			<td class="matrixOdd"><a href="http://mfung.scripts.mit.edu/LucCo/" class="team_site">mfung.scripts.mit.edu/LucCo</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd">PolyToons</td>
 			<td class="matrixEven">Wesam Manassra<br /> Saher Ahwal</td>
-			<td class="matrixOdd"><a href="http://manassra.scripts.mit.edu/">manassra.scripts.mit.edu</a></td>
+			<td class="matrixOdd"><a href="http://manassra.scripts.mit.edu/" class="team_site">manassra.scripts.mit.edu</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd">Scholar Sheep</td>
 			<td class="matrixEven">Felix Sung<br /> Alexander Jinhoon Lin</td>
-			<td class="matrixOdd"><a href="http://scholarsheep.herokuapp.com/">scholarsheep.herokuapp.com</a></td>
+			<td class="matrixOdd"><a href="http://scholarsheep.herokuapp.com/" class="team_site">scholarsheep.herokuapp.com</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd last">Tango</td>
 			<td class="matrixEven last">Jessica Lin<br />
 			Rui Luo</td>
-			<td class="matrixOdd last"><a href="http://ray_li.scripts.mit.edu/tango">ray_li.scripts.mit.edu/tango</a></td>
+			<td class="matrixOdd last"><a href="http://ray_li.scripts.mit.edu/tango" class="team_site">ray_li.scripts.mit.edu/tango</a></td>
 		</tr>
 </table>
 <br />
@@ -161,18 +161,18 @@ $('a').attr('target', '_blank');
 			<td class="matrixEven">Leah Alpert<br />
 			Russell Cohen<br />
 			Ram Bhaskaramurthi</td>
-			<td class="matrixOdd"><a href="http://rcoh.scripts.mit.edu/gradsearch/website/">rcoh.scripts.mit.edu/gradsearch/website</a></td>
+			<td class="matrixOdd"><a href="http://rcoh.scripts.mit.edu/gradsearch/website/" class="team_site">rcoh.scripts.mit.edu/gradsearch/website</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd">In The Loop</td>
 			<td class="matrixEven">Zack Drach<br /> Pasha Muravyev</td>
-			<td class="matrixOdd"><a href="http://li327-143.members.linode.com/">li327-143.members.linode.com</a></td>
+			<td class="matrixOdd"><a href="http://li327-143.members.linode.com/" class="team_site">li327-143.members.linode.com</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd last">Tango</td>
 			<td class="matrixEven last">Jessica Lin<br />
 			Rui Luo</td>
-			<td class="matrixOdd last"><a href="http://ray_li.scripts.mit.edu/tango">ray_li.scripts.mit.edu/tango</a></td>
+			<td class="matrixOdd last"><a href="http://ray_li.scripts.mit.edu/tango" class="team_site">ray_li.scripts.mit.edu/tango</a></td>
 		</tr>
 </table>
 <br />
@@ -192,6 +192,6 @@ $('a').attr('target', '_blank');
 			<td class="matrixEven last">Sharon Hao<br />
 			Britt Cyr<br />
 			Tim Donegan</td>
-			<td class="matrixOdd last"><a href="http://chooseyourchief.com/home.php">www.chooseyourchief.com/home.php</a></td>
+			<td class="matrixOdd last"><a href="http://chooseyourchief.com/home.php" class="team_site">www.chooseyourchief.com/home.php</a></td>
 		</tr>
 </table>
