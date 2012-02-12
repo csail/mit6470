5059a3624e4a961bafd51799c1f44be2007e516d
Joseph Laurendi <jlaurendi@gmail.com>
2012-02-12 10:27:27 -0500

    Updated awards list

--- 2012/application/views/competition/awards.php
+++ 2012/application/views/competition/awards.php
@@ -1,5 +1,5 @@
 <!-- Title / Page Headline -->
-<h1 class="headline center"><strong>Judging</strong></h1>
+<h1 class="headline center"><strong>Final Awards</strong></h1>
 
 <div class="hr"></div>
 <style>
@@ -8,10 +8,52 @@
 div#fancy_bg { background-color: #f0eef9; }
 div#fancy_div { background-color: #f0eef9; color: black; }
 div.lecture_details_div h2 { text-align: center;}
+h2.award_header { text-align: center; font-weight: bold; margin-top: 20px;}
 </style>
 <script>
 $('a').attr('target', '_blank');
 </script>
+
+<h2 class="award_header">Top Awards</h2>
+
+<table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
+	style="margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<th><h6>Place</h6></th>
+			<th><h6>Team Name</h6></th>
+			<th><h6>Team Members</h6></th>
+			<th><h6>Website</h6></th>
+		</tr>
+		<tr>
+			<td class="matrixOdd">1st</td>
+			<td class="matrixEven">Asler</td>
+			<td class="matrixOdd">Stephan Boyer<br /> Rachel Fong <br/> Mark Wittels</td>
+			<td class="matrixEven"><a href="http://asler.org/">www.asler.org</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">2nd</td>
+			<td class="matrixEven">In The Loop</td>
+			<td class="matrixOdd">Zack Drach<br /> Pasha Muravyev</td>
+			<td class="matrixEven"><a href="http://li327-143.members.linode.com/">li327-143.members.linode.com</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">3rd</td>
+			<td class="matrixEven">CollegeChef</td>
+			<td class="matrixOdd">Stephanie Chang<br /> Stephanie Yu<br /> Victor Hung</td>
+			<td class="matrixEven"><a href="http://niwen.scripts.mit.edu/collegechef/tutorials">niwen.scripts.mit.edu/collegechef/tutorials</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd last">4th</td>
+			<td class="matrixEven last">LearnTo</td>
+			<td class="matrixOdd last">Brodrick Dunlap Childs<br /> Oleksandr Stubailo<br /> Jeffrey Tyler Warren</td>
+			<td class="matrixEven last"><a href="http://learnto.herokuapp.com/">learnto.herokuapp.com</a></td>
+		</tr>
+</table>
+<br />
+
+<h2 class="award_header">Honorable Mention Tier 1</h2>
+
 <table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
 	style="margin-left: auto; margin-right: auto;">
 	<tbody>
@@ -21,72 +63,9 @@ $('a').attr('target', '_blank');
 			<th><h6>Website</h6></th>
 		</tr>
 		<tr>
-			<td class="matrixOdd">Asler</td>
-			<td class="matrixEven">Stephan Boyer<br /> Rachel Fong <br/> Mark Wittels</td>
-			<td class="matrixOdd"><a href="http://asler.org/">www.asler.org</a></td>
-		</tr>
-		<tr>
-			<td class="matrixOdd">PolyToons</td>
-			<td class="matrixEven">Wesam Manassra<br /> Saher Ahwal</td>
-			<td class="matrixOdd"><a href="http://manassra.scripts.mit.edu/">manassra.scripts.mit.edu</a></td>
-		</tr>
-		<tr>
-			<td class="matrixOdd">LucCo</td>
-			<td class="matrixEven">Michelle Fung<br /> Quanquan Liu</td>
-			<td class="matrixOdd"><a href="http://mfung.scripts.mit.edu/LucCo/">mfung.scripts.mit.edu/LucCo</a></td>
-		</tr>
-		<tr>
-			<td class="matrixOdd">Art Zone</td>
-			<td class="matrixEven">Cristina Lozano<br /> Sara Brockmueller</td>
-			<td class="matrixOdd"><a href="http://sarab609.scripts.mit.edu/">sarab609.scripts.mit.edu</a></td>
-		</tr>
-		<tr>
-			<td class="matrixOdd">Scholar Sheep</td>
-			<td class="matrixEven">Felix Sung<br /> Alexander Jinhoon Lin</td>
-			<td class="matrixOdd"><a href="http://scholarsheep.herokuapp.com/">scholarsheep.herokuapp.com</a></td>
-		</tr>
-		<tr>
-			<td class="matrixOdd">In The Loop</td>
-			<td class="matrixEven">Zack Drach<br /> Pasha Muravyev</td>
-			<td class="matrixOdd"><a href="http://li327-143.members.linode.com/">li327-143.members.linode.com</a></td>
-		</tr>
-		<tr>
-			<td class="matrixOdd">College Chef</td>
-			<td class="matrixEven">Stephanie Chang<br /> Stephanie Yu<br /> Victor Hung</td>
-			<td class="matrixOdd"><a href="http://niwen.scripts.mit.edu/collegechef/tutorials">niwen.scripts.mit.edu/collegechef/tutorials</a></td>
-		</tr>
-		<tr>
-			<td class="matrixOdd">Hippol</td>
-			<td class="matrixEven">Andi Wang<br /> Xunjie Li<br /> Ruwen Liu</td>
-			<td class="matrixOdd"><a href="http://ruwenliu.scripts.mit.edu/">ruwenliu.scripts.mit.edu</a></td>
-		</tr>
-		<tr>
 			<td class="matrixOdd">ProtoM</td>
 			<td class="matrixEven">Andrew Correa<br /> Jeremy Scott<br /> Ying Yin</td>
 			<td class="matrixOdd"><a href="http://miscoe.csail.mit.edu/">miscoe.csail.mit.edu</a></td>
-		</tr>
-		<tr>
-			<td class="matrixOdd">VoteNotes</td>
-			<td class="matrixEven">Andres Romero<br /> Vo Thanh Minh Tue<br /> Paul Hemberger</td>
-			<td class="matrixOdd"><a href="http://minhtuev.scripts.mit.edu/">minhtuev.scripts.mit.edu</a></td>
-		</tr>
-		<tr>
-			<td class="matrixOdd">LearnTo</td>
-			<td class="matrixEven">Brodrick Dunlap Childs<br /> Oleksandr Stubailo<br /> Jeffrey Tyler Warren</td>
-			<td class="matrixOdd"><a href="http://learnto.herokuapp.com/">learnto.herokuapp.com</a></td>
-		</tr>
-		<tr>
-			<td class="matrixOdd">Tango</td>
-			<td class="matrixEven">Jessica Lin<br />
-			Rui Luo</td>
-			<td class="matrixOdd"><a href="http://ray_li.scripts.mit.edu/tango">ray_li.scripts.mit.edu/tango</a></td>
-		</tr>
-		<tr>
-			<td class="matrixOdd">Choose Your Chief</td>
-			<td class="matrixEven">Sharon Hao<br />
-			Britt Cyr<br />
-			Tim Donegan</td>
-			<td class="matrixOdd"><a href="http://chooseyourchief.com/home.php">www.chooseyourchief.com/home.php</a></td>
 		</tr>
 		<tr>
 			<td class="matrixOdd">re:search</td>
@@ -103,10 +82,116 @@ $('a').attr('target', '_blank');
 			<td class="matrixOdd"><a href="http://www.viewerverse.com">www.viewerverse.com</a></td>
 		</tr>
 		<tr>
+			<td class="matrixOdd last">VoteNotes</td>
+			<td class="matrixEven last">Andres Romero<br /> Vo Thanh Minh Tue<br /> Paul Hemberger</td>
+			<td class="matrixOdd last"><a href="http://minhtuev.scripts.mit.edu/">minhtuev.scripts.mit.edu</a></td>
+		</tr>		
+</table>
+<br />
+
+<h2 class="award_header">Honorable Mention Tier 2</h2>
+
+<table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
+	style="margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<th><h6>Team Name</h6></th>
+			<th><h6>Team Members</h6></th>
+			<th><h6>Website</h6></th>
+		</tr>
+		<tr>
+			<td class="matrixOdd">Art Zone</td>
+			<td class="matrixEven">Cristina Lozano<br /> Sara Brockmueller</td>
+			<td class="matrixOdd"><a href="http://sarab609.scripts.mit.edu/">sarab609.scripts.mit.edu</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">Choose Your Chief</td>
+			<td class="matrixEven">Sharon Hao<br />
+			Britt Cyr<br />
+			Tim Donegan</td>
+			<td class="matrixOdd"><a href="http://chooseyourchief.com/home.php">www.chooseyourchief.com/home.php</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">Hippol</td>
+			<td class="matrixEven">Andi Wang<br /> Xunjie Li<br /> Ruwen Liu</td>
+			<td class="matrixOdd"><a href="http://ruwenliu.scripts.mit.edu/">ruwenliu.scripts.mit.edu</a></td>
+		</tr>
+		<tr>
 			<td class="matrixOdd last">LessonDrop</td>
 			<td class="matrixEven last">Shri Ganeshram<br />
 			Mark Fayngersh</td>
 			<td class="matrixOdd last"><a href="http://www.lessondrop.com">www.lessondrop.com</a></td>
 		</tr>
-	</tbody>
+		<tr>
+			<td class="matrixOdd">LucCo</td>
+			<td class="matrixEven">Michelle Fung<br /> Quanquan Liu</td>
+			<td class="matrixOdd"><a href="http://mfung.scripts.mit.edu/LucCo/">mfung.scripts.mit.edu/LucCo</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">PolyToons</td>
+			<td class="matrixEven">Wesam Manassra<br /> Saher Ahwal</td>
+			<td class="matrixOdd"><a href="http://manassra.scripts.mit.edu/">manassra.scripts.mit.edu</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">Scholar Sheep</td>
+			<td class="matrixEven">Felix Sung<br /> Alexander Jinhoon Lin</td>
+			<td class="matrixOdd"><a href="http://scholarsheep.herokuapp.com/">scholarsheep.herokuapp.com</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd last">Tango</td>
+			<td class="matrixEven last">Jessica Lin<br />
+			Rui Luo</td>
+			<td class="matrixOdd last"><a href="http://ray_li.scripts.mit.edu/tango">ray_li.scripts.mit.edu/tango</a></td>
+		</tr>
+</table>
+<br />
+
+<h2 class="award_header">MIT Utility Awards</h2>
+
+<table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
+	style="margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<th><h6>Team Name</h6></th>
+			<th><h6>Team Members</h6></th>
+			<th><h6>Website</h6></th>
+		</tr>
+		<tr>
+			<td class="matrixOdd">re:search</td>
+			<td class="matrixEven">Leah Alpert<br />
+			Russell Cohen<br />
+			Ram Bhaskaramurthi</td>
+			<td class="matrixOdd"><a href="http://rcoh.scripts.mit.edu/gradsearch/website/">rcoh.scripts.mit.edu/gradsearch/website</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">In The Loop</td>
+			<td class="matrixEven">Zack Drach<br /> Pasha Muravyev</td>
+			<td class="matrixOdd"><a href="http://li327-143.members.linode.com/">li327-143.members.linode.com</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd last">Tango</td>
+			<td class="matrixEven last">Jessica Lin<br />
+			Rui Luo</td>
+			<td class="matrixOdd last"><a href="http://ray_li.scripts.mit.edu/tango">ray_li.scripts.mit.edu/tango</a></td>
+		</tr>
+</table>
+<br />
+
+<h2 class="award_header">Audience Choice Award</h2>
+
+<table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
+	style="margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<th><h6>Team Name</h6></th>
+			<th><h6>Team Members</h6></th>
+			<th><h6>Website</h6></th>
+		</tr>
+		<tr>
+			<td class="matrixOdd last">Choose Your Chief</td>
+			<td class="matrixEven last">Sharon Hao<br />
+			Britt Cyr<br />
+			Tim Donegan</td>
+			<td class="matrixOdd last"><a href="http://chooseyourchief.com/home.php">www.chooseyourchief.com/home.php</a></td>
+		</tr>
 </table>
