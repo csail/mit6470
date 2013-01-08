abbf4ba473fc76d78ba7492aacf78ca84bf5c7aa
Joseph Laurendi <jlaurendi@gmail.com>
2013-01-08 11:47:17 -0500

    Made changes to schedule + css exercise

--- 2013/application/views/competition/schedule.php
+++ 2013/application/views/competition/schedule.php
@@ -3,10 +3,10 @@
 
 <div class="hr"></div>
 <style>
-#FeatureMatrix tr td { vertical-align: middle; }
-div#fancy_bg { background-color: #f0eef9; }
-div#fancy_div { background-color: #f0eef9; color: black; }
-div.lecture_details_div h2 { text-align: center;}
+#FeatureMatrix tr td { vertical-align:middle; }
+div#fancy_bg { background-color:#f0eef9; }
+div#fancy_div { background-color:#f0eef9; color:black; overflow:scroll;}
+div.lecture_details_div h2 { text-align:center;}
 </style>
 The schedule is subject to small changes so please be on the lookout for announcements. 
 <table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
@@ -72,7 +72,18 @@ The schedule is subject to small changes so please be on the lookout for announc
 			<td class="matrixItem">Thursday 1/10/13</td>
 			<td class="matrixOdd">10:00am - 11:00am</td>
 			<td class="matrixEven">10-250</td>
-			<td class="matrixOdd">Michael Lopp: The Engineer, The Designer, and the Dictator</td>
+			<td class="matrixOdd">
+				<a class="lecture_details_link" href="#palantir_details">Michael Lopp: The Engineer, The Designer, and the Dictator</a>
+				<div style="display:none;" id="palantir_details" class="lecture_details_div">
+					<h2 style="text-align: center;">The Engineer, The Designer, and the Dictator</h2>
+					<h3>Synopsis</h3>
+					<p>During the course of your career, it is likely you will have an undeniable urge to build a thing. It is equally likely that while you are well-intentioned, you are horrifically bad at a skill that is essential to successful thing building. In this talk, Michael Lopp will discuss ideal team construction and product design in a presentation called "The Engineer, The Designer, and the Dictator".</p>
+
+					<h3>Bio</h3>
+					<p>Michael Lopp is a Silicon Valley-based engineering manager who builds both people and software at companies such s Borland, Netscape, Apple and, most recently, Palantir. When he's not worrying about staying relevant, he writes about pens, bridges, people, poker, and werewolves at the popular weblog, Rands in Repose. Michael's second book "Being Geek" is a career handbook for geeks and nerds alike. His first book "Managing Humans" is a popular guide to the art of engineering management and clearly explains that while you might be rewarded for what you produce, you will only be successful because of your people.</p>
+					<p>Michael is currently a Director of Engineering at Palantir.</p>
+				</div>
+			</td>
 			<td class="matrixEven">Palantir Guest Lecture</td>
 			</td>
 		</tr>
@@ -80,7 +91,14 @@ The schedule is subject to small changes so please be on the lookout for announc
 			<td class="matrixItem"><br /></td>
 			<td class="matrixOdd">11:00am - 12:00pm</td>
 			<td class="matrixEven">10-250</td>
-			<td class="matrixOdd">Engineering Good Design and Usability</td>
+			<td class="matrixOdd">
+				<a class="lecture_details_link" href="#palantir2_details">Engineering Good Design & Usability</a>
+				<div style="display:none;" id="palantir2_details" class="lecture_details_div">
+					<h2 style="text-align: center;">Engineering Good Design and Usability</h2>
+					<h3>Synopsis</h3>
+					<p>Blake Reary and Cai GoGwilt from Palantir will be giving an in-depth lecture on what "good design" is and how to build a website with good design and usability in mind. They'll go through a concrete example of a website they prototyped as well as giving an overview of various topics such as prototyping, typography, layout, color, and grid systems.</p>
+				</div>
+			</td>
 			<td class="matrixEven">Palantir Guest Lecture</td>
 		</tr>
     <tr>
@@ -118,7 +136,14 @@ The schedule is subject to small changes so please be on the lookout for announc
 			<td class="matrixOdd"><?php echo HTML::anchor('competition/milestones#milestone1', 'Milestone 1 Due'); ?></td>
 			<td class="matrixEven"></td>
 		</tr>
-    <tr>
+		<tr>
+			<tr>
+				<td class="matrixItem">Tuesday 1/15/13</td>
+				<td class="matrixOdd">11:00am - 1:00pm</td>
+				<td class="matrixEven">10-250</td>
+				<td class="matrixOdd">Security</td>
+				<td class="matrixEven"></td>
+			</tr>
 			<td class="matrixItem">Tuesday 1/15/13</td>
 			<td class="matrixOdd">7:00pm - 9:00pm</td>
 			<td class="matrixEven">W20-500</td>
@@ -129,14 +154,29 @@ The schedule is subject to small changes so please be on the lookout for announc
 			<td class="matrixItem">Wednesday 1/16/13</td>
 			<td class="matrixOdd">11:00am - 1:00pm</td>
 			<td class="matrixEven">10-250</td>
-			<td class="matrixOdd">Web Security</td>
-			<td class="matrixEven">Advanced Topic Lecture</td>
+			<td class="matrixOdd">
+				<a class="lecture_details_link" href="#appian_details">Build it Bigger/Faster/Better with Modular Software Architecture</a>
+				<div style="display:none;" id="appian_details" class="lecture_details_div">
+					<h2 style="text-align: center;">Build it Bigger/Faster/Better with Modular Software Architecture</h2>
+					<h3>Bio</h3>
+					<p>Dan Mascenik is Appian's Vice President of Engineering. Dan oversees the design and development of the Appian platform, and is responsible for the day-to-day operations of the Technology department. He has over 12 years of experience with cloud-based enterprise software platforms. Dan holds a bachelor’s degree in engineering from the University of Michigan, Ann Arbor, and a master’s degree from Johns Hopkins University in Baltimore.</p> 
+				</div>
+			</td>
+			<td class="matrixEven">Appian Guest Lecture</td>
     </tr>
 		<tr>
 			<td class="matrixItem"><br /></td>
 			<td class="matrixOdd">1:00pm - 2:00pm</td>
 			<td class="matrixEven">10-250</td>
-			<td class="matrixOdd">TBD</td>
+			<td class="matrixOdd">
+				<a class="lecture_details_link" href="#dropbox_details">A severely opinionated overview of the Dropbox web stack</a>
+				<div style="display:none;" id="dropbox_details" class="lecture_details_div">
+					<h2 style="text-align: center;">A severely opinionated overview of the Dropbox web stack</h2>
+					<h3>Synopsis</h3>
+					<p>Dan Wheeler (MIT alum) will be giving a whirlwind tour of Dropbox's web infrastructure. Dropbox, one of the hottest startups of the past 10 years, has been able to scale their development team while maintaining incredible agility. Come learn about the technologies and processes they use to make this possible.</p>
+				</div>
+				
+			</td>
 			<td class="matrixEven">Dropbox Guest Lecture</td>
     </tr>
 		<tr>
@@ -152,13 +192,6 @@ The schedule is subject to small changes so please be on the lookout for announc
 			<td class="matrixEven">10-250</td>
 			<td class="matrixOdd">TBD</td>
 			<td class="matrixEven">Vecna Guest Lecture</td>
-    </tr>
-		<tr>
-			<td class="matrixItem"><br /></td>
-			<td class="matrixOdd">1:00pm - 2:00pm</td>
-			<td class="matrixEven">10-250</td>
-			<td class="matrixOdd">TBD</td>
-      <td class="matrixEven">Quora Guest Lecture</td>
     </tr>
     <tr>
 			<td class="matrixItem"><br /></td>
