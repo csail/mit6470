1665fa75c1e703593eff07ce02edbed12f909ba7
Mark Zhang <mazhang@mit.edu>
2013-01-07 19:26:37 -0500

    Added 2013 competition themes

--- 2013/application/views/competition/challenge.php
+++ 2013/application/views/competition/challenge.php
@@ -3,11 +3,70 @@
 
 <div class="hr"></div>
 
-<h2>The challenge for 2013 will be released on the first day of IAP!</h2>
-
 <!-- <p>The topics for this year's competition are:</p>
 <ul>
     <li>Politics</li>
     <li>Education - we're looking for more along the lines of utilities/applications that enable people to learn. Think along the lines of flashcards, a quiz-like game, or an online terminal that allows people to learn programming.</li>
     <li>TV</li>
 </ul> -->
+
+<p>Your site must match one of the following three themes to qualify for the competition. If your site meets the technical requirements but does not match the themes, you will receive academic credit for the course, but will not qualify for the competition.</p>
+
+<p>For each of the following themes, we have provided several examples, as well as a counterexample. The examples are designed to be vague enough to allow for many different designs and implementations, so do not feel that the examples are in any way "off-limits".</p>
+
+<h2>Theme 1: Vacations</h2>
+
+<p>Design a website that helps users to <b>plan or document their vacations</b>. Your website cannot focus on commercial transactions, such as the purchase or reservation of hotel rooms, plane/bus tickets, or renting cars.</p>
+
+<p>Examples:</p>
+
+<ul>
+<li>A website that filters vacation destinations based on user interests.</li>
+<li>A collaborative website where users build a single article for each popular destination detailing key information like cultural information and travel tips.</li>
+<li>A website that allows users to specify their dream vacation spots and then matches them with friends with similar preferences.</li>
+<li>A online social travel journal that lets users document their past vacations.</li>
+</ul>
+
+<p>A counterexample:</p>
+<ul>
+<li>A website that helps you book flights or hotels.</li>
+</ul>
+
+<h2>Theme 2: Real-time Education</h2>
+
+<p>Design a website that <b>facilitates dynamic, real-time interactions in a classroom setting</b>. This includes settings such as lectures, recitations, and discussion-based classes, and is not limited to settings at MIT. Your website can include extra features to be used outside of class (for example, for a collaborative note-taking website, allowing the students to access the notes after class), but the core site concept must revolve around something that happens in-class.</p>
+
+<p>Examples:</p>
+
+<ul>
+<li>A website that allows students to ask questions during lecture and upvote other students' questions. Instructors can answer popular questions at their own pace during the lecture.</li>
+<li>A website that facilitates collaborative note taking.</li>
+<li>A website that allows the instructor to give real-time exercises to the class during the lecture and get feedback on the results.</li>
+</ul>
+<p>A counterexample:</p>
+<ul>
+<li>A website that does not focus on real-time in-classroom interactions, such as a course management system like Stellar.<li>
+</ul>
+
+<p>Additional information:</p>
+<p>We expect that many of our competitors will have limited teaching experience. We recommend reading <a href='http://ocw.mit.edu/resources/res-18-004-the-torch-or-the-firehose-a-guide-to-section-teaching-spring-2009/index.htm'>MIT's TA handbook</a>, which will both expose students to challenges that educators face and possibly provide inspiration for a site concept. The handbook is directed towards TAs but your professors face similar challenges. Additionally, it may be valuable to interview a professor or lecturer during Milestone 1 if you wish to pursue this theme.</p>
+
+<h2>Theme 3: Meeting New People</h2>
+
+<p>Design a website that <b>facilitates meeting new people</b>. The action of meeting new people should be the central focus of your site. That is, users should be joining your site with the intent of meeting new people in some way.</p>
+
+<p>Examples:</p>
+<ul>
+<li>A website that allows you to easily find and meet people with similar interests in your area.</li>
+<li>A dating website.</li>
+<li>A website that allows you to chat with random people on the Internet.</li>
+</ul>
+
+<p>A counterexample:</p>
+<ul>
+<li>A website where you primarily interact with people that you already know, such as Facebook. In general, social networks are not prohibited but need to focus on meeting new people as the core purpose of the website.</li>
+</ul>
+
+<p>Additional information:</p>
+<p>If you decide to pursue this theme, please be aware of socially harmful behaviors (such as stalking or bullying) that social websites sometimes accidentally facilitate. Be thinking about ways that people may misuse your website, and try to design your website/feature set to discourage or minimize such negative or harmful behaviors.</p>
+
