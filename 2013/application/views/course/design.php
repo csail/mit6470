3ffc33b5b1905d16037d96a596de027a5670e421
Joseph Laurendi <jlaurendi@gmail.com>
2013-01-14 11:48:30 -0500

    Added palantir guest lecture

--- 2013/application/views/course/design.php
+++ 2013/application/views/course/design.php
@@ -6,5 +6,9 @@
 <h2 class="headline center "><strong>Reading List</strong></h2>
 <p class="flavor_text">
 Click here to download the <?php echo HTML::anchor('assets/resources/DesignReadingList.pdf', 'Design Reading List'); ?>
+
 </p>
 
+<p>
+	Also check out <?php echo HTML::anchor('assets/resources/designforeng_01_12.pdf', "Palantir's guest lecture on design"); ?>.
+</p>
