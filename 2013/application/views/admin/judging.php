0e854be343d4689b486532bc2fd5bd3aedf22f1b
Joseph Laurendi <jlaurendi@gmail.com>
2013-01-30 14:17:01 -0500

    Added semifinalists emails

--- 2013/application/views/admin/judging.php
+++ 2013/application/views/admin/judging.php
@@ -18,36 +18,61 @@ div.lecture_details_div h2 { text-align: center;}
 			<th><h6>Team Name</h6></th>
 			<th><h6>Team Members</h6></th>
 			<th><h6>Website</h6></th>
+			<th><h6>Emails</h6></th>
 		</tr>
 		<tr>
 			<td class="matrixItem">11:15 AM</td>
 			<td class="matrixOdd">Test#Code</td>
-			<td class="matrixEven">Shantanu Jain<br /> Andrei Ivanov <br/> Dumitru Savva</td>
+			<td class="matrixEven">Shantanu Jain <br /> Andrei Ivanov <br/> Dumitru Savva </td>
 			<td class="matrixOdd"><a href="http://jains.scripts.mit.edu/testcode/">http://jains.scripts.mit.edu/testcode/</a></td>
+			<td class="matrixEven">
+				jains@mit.edu <br />
+				aivanov@mit.edu <br />
+				dimasavva17@gmail.com
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">11:30 AM</td>
 			<td class="matrixOdd">SubmitQ</td>
 			<td class="matrixEven">Dong Hyug Lim <br /> Harlin Lee <br/> Clare Liu </td>
 			<td class="matrixOdd"><a href="http://harlin.scripts.mit.edu/submitq/">http://harlin.scripts.mit.edu/submitq/</a></td>
+			<td class="matrixEven">
+				dhlim@mit.edu <br />
+				harlin@mit.edu <br />
+				clareliu@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">11:45 AM</td>
 			<td class="matrixOdd">DreamTrip</td>
 			<td class="matrixEven">Srinidhi Viswanathan <br /> Jeevana Inala </td>
 			<td class="matrixOdd"><a href="http://jinala.scripts.mit.edu/tripideas.php">http://jinala.scripts.mit.edu/tripideas.php</a></td>
+			<td class="matrixEven">
+				srinidhi@mit.edu <br />
+				jinala@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">12:00 PM</td>
 			<td class="matrixOdd">Apparate</td>
 			<td class="matrixEven">Yunzhi Gao <br /> Yihua Li <br/> Yixin Li </td>
 			<td class="matrixOdd"><a href="http://liyihua.scripts.mit.edu/">http://liyihua.scripts.mit.edu/</a></td>
+			<td class="matrixEven">
+				gyz@mit.edu <br />
+				liyihua@mit.edu <br />
+				liyixin@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem last">12:15 PM</td>
 			<td class="matrixOdd last">Travelog</td>
 			<td class="matrixEven last"> Lucy Yu <br /> Deniz Oktay <br/> William Qian </td>
 			<td class="matrixOdd last"><a href="http://wqian94.scripts.mit.edu/DWL/">http://wqian94.scripts.mit.edu/DWL/</a></td>
+			<td class="matrixEven last">
+				lucyyu@mit.edu <br />
+				denizokt@mit.edu <br />
+				wqian94@mit.edu
+			</td>
 		</tr>
 	</tbody>
 </table>
@@ -62,84 +87,140 @@ div.lecture_details_div h2 { text-align: center;}
 			<th><h6>Team Name</h6></th>
 			<th><h6>Team Members</h6></th>
 			<th><h6>Website</h6></th>
+			<th><h6>Emails</h6></th>
 		</tr>
 		<tr>
 			<td class="matrixItem">1:00 PM</td>
 			<td class="matrixOdd">Jellify</td>
 			<td class="matrixEven">Alexander Lin <br /> Felix Sung <br/> Kimberly Toy </td>
 			<td class="matrixOdd"><a href="http://jellify.herokuapp.com/">http://jellify.herokuapp.com/</a></td>
+			<td class="matrixEven">
+				ajlin@mit.edu <br />
+				fenix@mit.edu <br />
+				toyk2a@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">1:15 PM</td>
 			<td class="matrixOdd">PsetParty</td>
 			<td class="matrixEven">Yinong Liu <br /> Staphany Park <br/> Geza Kovacs </td>
 			<td class="matrixOdd"><a href="http://psetparty.xvm.mit.edu:3333/login.html">http://psetparty.xvm.mit.edu:3333/login.html</a></td>
+			<td class="matrixEven">
+				yinong@mit.edu <br />
+				spark008@mit.edu <br />
+				gkovacs@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">1:30 PM</td>
 			<td class="matrixOdd">Notability</td>
 			<td class="matrixEven"> Oliver R. Song </td>
 			<td class="matrixOdd"><a href="http://www.notability.org/">http://www.notability.org/</a></td>
+			<td class="matrixEven">
+				osong@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">1:45 PM</td>
 			<td class="matrixOdd">Trippr</td>
 			<td class="matrixEven"> Benjamin Frank <br /> Jonathan Wang <br/> Ariel Wexler </td>
 			<td class="matrixOdd"><a href="http://trippr.info">trippr.info</a></td>
+			<td class="matrixEven">
+				bfrank@mit.edu <br />
+				jwang7@mit.edu <br />
+				arikwex@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">2:00 PM</td>
 			<td class="matrixOdd">Happenline</td>
 			<td class="matrixEven"> Connor Kirschbaum </td>
 			<td class="matrixOdd"><a href="http://ckbaum.scripts.mit.edu/happenline/">http://ckbaum.scripts.mit.edu/happenline/</a></td>
+			<td class="matrixEven">
+				ckbaum@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">2:15 PM</td>
 			<td class="matrixOdd">Lapine</td>
 			<td class="matrixEven"> Ming Yang Ong <br /> Aidan Bevacqua <br/> Leon Lin </td>
 			<td class="matrixOdd"><a href="http://edu.xvm.mit.edu/">http://edu.xvm.mit.edu/</a></td>
+			<td class="matrixEven">
+				leonxlin@mit.edu <br />
+				bevacqua@mit.edu <br />
+				mingy@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">2:45 PM</td>
 			<td class="matrixOdd">YourTale</td>
 			<td class="matrixEven"> Dalton Hubble <br/> Thomas Garver </td>
 			<td class="matrixOdd"><a href="http://yourtale.herokuapp.com/">http://yourtale.herokuapp.com/</a></td>
+			<td class="matrixEven">
+				dhubble@mit.edu <br />
+				tgarv@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">3:00 PM</td>
 			<td class="matrixOdd">liveClass</td>
 			<td class="matrixEven"> Tahin Fahmid Syed </td>
 			<td class="matrixOdd"><a href="http://18.95.6.81:3000/main">http://18.95.6.81:3000/main</a></td>
+			<td class="matrixEven">
+				syedt@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">3:15 PM</td>
 			<td class="matrixOdd">Atlas</td>
 			<td class="matrixEven"> Jorge Jay-Jay Amaya <br/> Alexander M. Dixon </td>
 			<td class="matrixOdd"><a href="http://agile-sands-2491.herokuapp.com/">http://agile-sands-2491.herokuapp.com/</a></td>
+			<td class="matrixEven">
+				jamaya@mit.edu <br />
+				amdixon@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">3:30 PM</td>
 			<td class="matrixOdd">Set-up</td>
 			<td class="matrixEven"> Sean Karson <br /> Cosmos Darwin </td>
 			<td class="matrixOdd"><a href="http://set-up.mit.edu/">http://set-up.mit.edu/</a></td>
+			<td class="matrixEven">
+				karsons@mit.edu <br />
+				cosmosd@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">3:45 PM</td>
 			<td class="matrixOdd">TrippedUp</td>
 			<td class="matrixEven">Danny Chiao <br /> Kuljot Anand <br/> David Byrne </td>
 			<td class="matrixOdd"><a href="http://trippedup.herokuapp.com/">http://trippedup.herokuapp.com/</a></td>
+			<td class="matrixEven">
+				dchiao@mit.edu <br />
+				anandk@mit.edu <br />
+				dbyrne@bu.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem">4:00 PM</td>
 			<td class="matrixOdd">Carbonate</td>
 			<td class="matrixEven"> Felix Sun <br /> Chelsea Voss </td>
 			<td class="matrixOdd"><a href="http://felixsun.scripts.mit.edu/orgo">felixsun.scripts.mit.edu/orgo</a></td>
+			<td class="matrixEven">
+				felixsun@mit.edu <br />
+				csvoss@mit.edu
+			</td>
 		</tr>
 		<tr>
 			<td class="matrixItem last">4:15 PM</td>
 			<td class="matrixOdd last">Ultramarine</td>
 			<td class="matrixEven last"> Sashko Stubailo <br /> Brodrick Childs <br/> Carolyn Zhang </td>
 			<td class="matrixOdd last"><a href="http://ultramarine.herokuapp.com/">http://ultramarine.herokuapp.com/</a></td>
+			<td class="matrixEven last">
+				sashko@mit.edu <br />
+				brodrick@mit.edu <br />
+				carolynz@mit.edu
+			</td>
 		</tr>
 	</tbody>
 </table>
