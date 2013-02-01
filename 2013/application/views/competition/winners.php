2555eb60841a8e1a103ed68dfd1bc462fcbd267c
Joseph Laurendi <jlaurendi@gmail.com>
2013-02-01 16:26:08 -0500

    Added winners

--- 2013/application/views/competition/winners.php
+++ 2013/application/views/competition/winners.php
@@ -1,5 +1,212 @@
+<script>
+$(function() {
+	$('a.team-site').attr('target', '_blank');	
+});
+</script>
+
 <!-- Title / Page Headline -->
 <h1 class="headline center"><strong>Winners 2013</strong></h1>
 <div class="hr"></div>
-<h2 style="line-height: 30px">The winners of 6.470 2013 will be announced during the Awards Ceremony on January 31, 2013 in 10-250, 7-10pm! Please join us for free food and awesome website presentations, and cheer for your favorite website for the Audience Choice Awards!</h2>
 
+
+<h2>Main Division</h2>
+<table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
+	style="margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<th><h6>Place</h6></th>
+			<th><h6>Team Name</h6></th>
+			<th><h6>Team Members</h6></th>
+			<th><h6>Website</h6></th>
+		</tr>
+		<tr>
+			<td class="matrixItem">1st</td>
+			<td class="matrixOdd">Jellify</td>
+			<td class="matrixEven">Alexander Lin <br /> Felix Sung <br/> Kimberly Toy </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://jellify.herokuapp.com/">http://jellify.herokuapp.com/</a></td>
+		</tr>
+		<tr>
+			<td class="matrixItem">2nd</td>
+			<td class="matrixOdd">Set-up</td>
+			<td class="matrixEven"> Sean Karson <br /> Cosmos Darwin </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://set-up.mit.edu/">http://set-up.mit.edu/</a></td>
+		</tr>
+		<tr>
+			<td class="matrixItem">3rd</td>
+			<td class="matrixOdd">Happenline</td>
+			<td class="matrixEven"> Connor Kirschbaum </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://ckbaum.scripts.mit.edu/happenline/">http://ckbaum.scripts.mit.edu/happenline/</a></td>
+		</tr>
+		<tr>
+			<td class="matrixItem last">4th</td>
+			<td class="matrixOdd last">Atlas</td>
+			<td class="matrixEven last"> Jorge Jay-Jay Amaya <br/> Alexander M. Dixon </td>
+			<td class="matrixOdd last"><a class="team-site"  href="http://agile-sands-2491.herokuapp.com/">http://agile-sands-2491.herokuapp.com/</a></td>
+		</tr>
+	</tbody>
+</table>
+<br />
+
+<h2>Main Division Honorable Mention Tier 1</h2>
+<table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
+	style="margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<th><h6>Team Name</h6></th>
+			<th><h6>Team Members</h6></th>
+			<th><h6>Website</h6></th>
+		</tr>
+		<tr>
+			<td class="matrixOdd">PsetParty</td>
+			<td class="matrixEven">Yinong Liu <br /> Staphany Park <br/> Geza Kovacs </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://psetparty.xvm.mit.edu:3333/login.html">http://psetparty.xvm.mit.edu:3333/login.html</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">Notability</td>
+			<td class="matrixEven"> Oliver R. Song </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://www.notability.org/">http://www.notability.org/</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">Lapine</td>
+			<td class="matrixEven"> Ming Yang Ong <br /> Aidan Bevacqua <br/> Leon Lin </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://edu.xvm.mit.edu/">http://edu.xvm.mit.edu/</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">YourTale</td>
+			<td class="matrixEven"> Dalton Hubble <br/> Thomas Garver </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://yourtale.herokuapp.com/">http://yourtale.herokuapp.com/</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd last">Carbonate</td>
+			<td class="matrixEven last"> Felix Sun <br /> Chelsea Voss </td>
+			<td class="matrixOdd last"><a class="team-site"  href="http://felixsun.scripts.mit.edu/orgo">felixsun.scripts.mit.edu/orgo</a></td>
+		</tr>
+	</tbody>
+</table>
+<br />
+
+<h2>Main Division Honorable Mention Tier 2</h2>
+<table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
+	style="margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<th><h6>Team Name</h6></th>
+			<th><h6>Team Members</h6></th>
+			<th><h6>Website</h6></th>
+		</tr>
+		<tr>
+			<td class="matrixOdd">Trippr</td>
+			<td class="matrixEven"> Benjamin Frank <br /> Jonathan Wang <br/> Ariel Wexler </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://trippr.info">trippr.info</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">liveClass</td>
+			<td class="matrixEven"> Tahin Fahmid Syed </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://18.95.6.81:3000/main">http://18.95.6.81:3000/main</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">TrippedUp</td>
+			<td class="matrixEven">Danny Chiao <br /> Kuljot Anand <br/> David Byrne </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://trippedup.herokuapp.com/">http://trippedup.herokuapp.com/</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd last">Ultramarine</td>
+			<td class="matrixEven last"> Sashko Stubailo <br /> Brodrick Childs <br/> Carolyn Zhang </td>
+			<td class="matrixOdd last"><a class="team-site"  href="http://ultramarine.herokuapp.com/">http://ultramarine.herokuapp.com/</a></td>
+		</tr>
+	</tbody>
+</table>
+<br />
+
+<h2>Rookie Division</h2>
+<table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
+	style="margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<th><h6>Place</h6></th>
+			<th><h6>Team Name</h6></th>
+			<th><h6>Team Members</h6></th>
+			<th><h6>Website</h6></th>
+		</tr>
+		<tr>
+			<td class="matrixItem">1st</td>
+			<td class="matrixOdd">Test#Code</td>
+			<td class="matrixEven">Shantanu Jain <br /> Andrei Ivanov <br/> Dumitru Savva </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://jains.scripts.mit.edu/testcode/">http://jains.scripts.mit.edu/testcode/</a></td>
+		</tr>
+		<tr>
+			<td class="matrixItem">2nd</td>
+			<td class="matrixOdd">SubmitQ</td>
+			<td class="matrixEven">Dong Hyug Lim <br /> Harlin Lee <br/> Clare Liu </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://harlin.scripts.mit.edu/submitq/">http://harlin.scripts.mit.edu/submitq/</a></td>
+		</tr>
+		<tr>
+			<td class="matrixItem last">3rd</td>
+			<td class="matrixOdd last">Apparate</td>
+			<td class="matrixEven last">Yunzhi Gao <br /> Yihua Li <br/> Yixin Li </td>
+			<td class="matrixOdd last"><a class="team-site"  href="http://liyihua.scripts.mit.edu/">http://liyihua.scripts.mit.edu/</a></td>
+		</tr>
+	</tbody>
+</table>
+<br />
+
+<h2>Rookie Division Honorable Mentions</h2>
+<table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
+	style="margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<th><h6>Team Name</h6></th>
+			<th><h6>Team Members</h6></th>
+			<th><h6>Website</h6></th>
+		</tr>
+		<tr>
+			<td class="matrixOdd">DreamTrip</td>
+			<td class="matrixEven">Srinidhi Viswanathan <br /> Jeevana Inala </td>
+			<td class="matrixOdd"><a class="team-site"  href="http://jinala.scripts.mit.edu/tripideas.php">http://jinala.scripts.mit.edu/tripideas.php</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd last">Travelog</td>
+			<td class="matrixEven last"> Lucy Yu <br /> Deniz Oktay <br/> William Qian </td>
+			<td class="matrixOdd last"><a class="team-site"  href="http://wqian94.scripts.mit.edu/DWL/">http://wqian94.scripts.mit.edu/DWL/</a></td>
+		</tr>
+	</tbody>
+</table>
+<br />
+
+<h2>Special Prizes</h2>
+<table cellspacing="0" cellpadding="0" id="FeatureMatrix" class="center" 
+	style="margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<th><h6>Prize</h6></th>
+			<th><h6>Team Name</h6></th>
+			<th><h6>Team Members</h6></th>
+			<th><h6>Website</h6></th>
+		</tr>
+		<tr>
+			<td class="matrixOdd">Staff Pick</td>
+			<td class="matrixEven">Trippr</td>
+			<td class="matrixOdd"> Benjamin Frank <br /> Jonathan Wang <br/> Ariel Wexler </td>
+			<td class="matrixEven"><a class="team-site"  href="http://trippr.info">trippr.info</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">Staff HM</td>
+			<td class="matrixEven">Notability</td>
+			<td class="matrixOdd"> Oliver R. Song </td>
+			<td class="matrixEven"><a class="team-site"  href="http://www.notability.org/">http://www.notability.org/</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd">MIT Utility</td>
+			<td class="matrixEven">Carbonate</td>
+			<td class="matrixOdd"> Felix Sun <br /> Chelsea Voss </td>
+			<td class="matrixEven"><a class="team-site"  href="http://felixsun.scripts.mit.edu/orgo">felixsun.scripts.mit.edu/orgo</a></td>
+		</tr>
+		<tr>
+			<td class="matrixOdd last">Staff Entertainment</td>
+			<td class="matrixEven last">Travelog</td>
+			<td class="matrixOdd last"> Lucy Yu <br /> Deniz Oktay <br/> William Qian </td>
+			<td class="matrixEven last"><a class="team-site"  href="http://wqian94.scripts.mit.edu/DWL/">http://wqian94.scripts.mit.edu/DWL/</a></td>
+		</tr>
+	</tbody>
+</table>
+<br />
